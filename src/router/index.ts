a7279aeb7ec02befd59934a9834e2c9b4861af0f
Pakho12138 <326061636@qq.com>
2025-01-06 23:30:42 +0800

    ci：部署

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import Home from '../views/home/index.vue';
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
